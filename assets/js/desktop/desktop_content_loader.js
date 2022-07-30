e944fcff6e126728681f5397c0ceed96005abe90
ChocoretoStonus <chocoretostonus@gmail.com>
2022-07-29 22:29:33 -0500

    Maybe all complete

--- assets/js/desktop/desktop_content_loader.js
+++ assets/js/desktop/desktop_content_loader.js
@@ -1,6 +1,7 @@
-var pq = new PandaRequest(5);
+var pq = new PandaRequest(6);
 pq.loadContent(q('#about'), 'content/about.html');
 pq.loadContent(q('#skills'), 'content/skills.html');
 pq.loadContent(q('#projects'), 'content/projects.html');
 pq.loadContent(q('#music'), 'content/music.html');
 pq.loadContent(q('#contact'), 'content/contact.html');
+pq.loadContent(q('#osu'), 'content/yavi.html');
